800b20b4d8f1cd7ae6bd6c0a1e63767e02da6a43
Danny Aziz <daanyaal997@gmail.com>
2023-12-18 16:26:30 -0500

    fix: use path instead of query params

--- src/index.ts
+++ src/index.ts
@@ -36,4 +36,34 @@ app.get('/', async (req: Request<{}, {}, {}, Parameters>, res: Response) => {
         });
     }
 });
+
+interface RouteParams {
+    arch: string;
+    version: string;
+}
+
+app.get('/darwin/:arch/:version', async (req: Request<RouteParams, {}, {}, Parameters>, res: Response) => {
+    if (req.params.arch) {
+        const releases = await github();
+        const latest = releases.find((release) => {
+            return release.tag_name?.startsWith(process.env.TAG_STRUCTURE);
+        });
+        if (latest) {
+            const version = process.env.TAG_STRUCTURE ? latest.tag_name.split(process.env.TAG_STRUCTURE)[1] : latest.tag_name;
+            const release = getReleases(latest.assets, 'darwin', req.params.arch);
+            if (Object.keys(release).length !== 0) {
+                res.status(200).send(await template(release, version, 'darwin'));
+            } else {
+                res.status(204).send();
+            }
+        } else {
+            res.status(204).send();
+        }
+    } else {
+        res.status(400).send({
+            message: 'Invalid request',
+        });
+    }
+});
+
 app.listen(parseInt(process.env.PORT, 10) || 8080, '0.0.0.0', () => console.log(`Server started at http://0.0.0.0:8080/`));
